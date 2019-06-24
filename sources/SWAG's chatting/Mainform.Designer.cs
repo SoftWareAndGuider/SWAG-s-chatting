10ccd8e763b7f0d51701d2722b566a2576b51af9
PMH Studio / CS <hj666c3@gmail.com>
2019-06-24 18:02:47 +0900

    누락

--- sources/SWAG's chatting/Mainform.Designer.cs
+++ sources/SWAG's chatting/Mainform.Designer.cs
@@ -28,13 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(24, 63);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(126, 292);
+            this.listBox1.TabIndex = 0;
             // 
             // Mainform
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(637, 378);
+            this.Controls.Add(this.listBox1);
             this.Font = new System.Drawing.Font("굴림", 9F);
             this.Margin = new System.Windows.Forms.Padding(0, 6, 0, 6);
             this.Name = "Mainform";
@@ -46,5 +57,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
